3a119bd6e96f2965000094c182af0928894f4435
Chris <cwriddle5@gmail.com>
2021-05-24 00:27:17 -0400

    working code. no input validation

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -19,56 +19,8 @@ public class FoodTruck {
 		setId(numTrucks);
 	}
 	
-	static void listExistingFoodTrucks(FoodTruck[] foodTrucks){
-		
-		String x = foodTrucks[0].getName();
-		
-		for(int i=0; i<numTrucks; i++) {
-			//System.out.println(foodTrucks[i].getName());
-			x = foodTrucks[i].getName();
-			if(x.length() > 0) {
-				toString(foodTrucks[i]);
-			}
-			else
-				continue;
-				//toString(foodTrucks[i]);
-		}
-		
-	}
-	
-	static void findAverageRating(FoodTruck[] foodTrucks) {
-		double sum = 0;
-		
-		for (int i=0; i<foodTrucks.length; i++) {
-			sum += foodTrucks[i].getRating();
-		}
-		double average = (sum / foodTrucks.length);
-		System.out.printf("The average rating of all food trucks is %.2f%n", average);
-		
-		//return average;
-		
-		
-//	for one version
-//		int sum = 0;
-//		double average;
-//		
-//		for(int i=0; i < foodTrucks.length; i++) {
-//			sum += foodTrucks[i].getRating();
-//		}
-//		average = (double) sum / foodTrucks.length;
-//		return average;
-	}
-	
-	static void findHighestRated(FoodTruck[] foodTrucks) {
-		
-		FoodTruck highestRated = foodTrucks[0];
-		for(int i=0; i<foodTrucks.length; i++) {
-			if(foodTrucks[i].getRating() > highestRated.getRating()) {
-				highestRated = foodTrucks[i];
-			}
-		}
-		System.out.println("The highest rated food truck is: ");
-		FoodTruck.toString(highestRated);
+	public static int getNumTrucks() {
+		return numTrucks;
 	}
 	
 	public String getName() {
@@ -104,11 +56,9 @@ public class FoodTruck {
 		return id;
 	}
 	
-	private static void toString(FoodTruck foodTruck) {
-		System.out.println("Food Truck ID: " + foodTruck.getId());
-		System.out.println("\tName: " + foodTruck.getName());
-		System.out.println("\tType of Food: " + foodTruck.getFoodType());
-		System.out.println("\tRating: " + foodTruck.getRating());
-		System.out.println();
+	public String toString() {
+		String result;
+		result = "+Food Truck ID: " + this.id + "\n|\tName: " + this.name + "\n|\tFood: " + this.foodType + "\n|\tRating: " + this.rating + "\n|";
+		return result;
 	}
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -7,64 +7,26 @@ public class FoodTruckApp {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
 		
-		System.out.println("This program will allow you to input the information and ratings for various food trucks.");
-		System.out.print("How many food trucks would you like to enter?: ");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		System.out.println("| This program will allow you to input the information and ratings for various food trucks. |");
+		System.out.println("---------------------------------------------------------------------------------------------");
+		System.out.println("|                                                                                            ");
+		System.out.println("+--------------------------------------------------------------------------------------------");
+		System.out.print("| How many food trucks would you like to enter?: ");
 		int foodTruckAmt = kb.nextInt();
+		System.out.println("+--------------------------------------------------------------------------------------------");
 		
 		FoodTruck[] foodTruck = new FoodTruck[foodTruckAmt];
-
-		initFoodTrucks(foodTruck, kb);
-		menu(kb, foodTruck);
-		
-		System.out.println("Program ended.");
-		kb.close();
-		
-	}
-	
-	public static void menu(Scanner kb, FoodTruck[] foodTruck) {
-		boolean looping = true;
-		
-		while(looping) {
-			
-			System.out.println("1. List all existing food trucks.");
-			System.out.println("2. See the average rating of food trucks.");
-			System.out.println("3. Display the highest-rated food truck.");
-			System.out.println("4. Quit the program.");
-			
-			int input = kb.nextInt();
-			
-			switch(input) {
-			case 1:
-				FoodTruck.listExistingFoodTrucks(foodTruck);
-				//listExistingFoodTrucks();
-				break;
-			case 2:
-				FoodTruck.findAverageRating(foodTruck);
-				//System.out.println(FoodTruck.findAverageRating(foodTruck));
-				//findAverageRating();
-				break;
-			case 3:
-				FoodTruck.findHighestRated(foodTruck);
-				//System.out.println(FoodTruck.findHighestRated(foodTruck).getRating());
-				//findHighestRated();
-				break;
-			case 4:
-				looping = false;
-				break;
-			}
-		}
-	}
-	
-	public static void initFoodTrucks(FoodTruck foodTruck[], Scanner kb) {
-		
+//
 		String inputStr;
 		int inputInt;
 		boolean looping = true;
 		
 		while(looping) {
 			for(int i=0; i<foodTruck.length; i++) {
-				System.out.print("Enter name of food truck #" + (i+1) + ": ");
-				inputStr = kb.next();
+				System.out.print("\n| Enter name of food truck #" + (i+1) + ": ");
+				kb.next();
+				inputStr = kb.nextLine();
 				if(inputStr.equals("quit")) {
 					looping = false;
 					break;
@@ -73,16 +35,85 @@ public class FoodTruckApp {
 					foodTruck[i] = new FoodTruck();	//initialized new food truck
 					foodTruck[i].setName(inputStr);
 				}
-				System.out.print("What type of food did they serve?: ");
-				inputStr = kb.next();
+				System.out.print("| What type of food did they serve?: ");
+				inputStr = kb.nextLine();
 				foodTruck[i].setFoodType(inputStr);
-				System.out.print("How many stars out of 5 do you give it?: ");
+				System.out.print("| How many stars out of 5 do you give it?: ");
 				inputInt = kb.nextInt();
 				foodTruck[i].setRating(inputInt);
 				kb.nextLine();
 			}
 			looping = false;
 		}
+//
+		looping = true;
+		
+		while(looping) {
+			System.out.println();
+			System.out.println("+--------------------------------------------+");
+			System.out.println("|                    MENU                    |");
+			System.out.println("+--------------------------------------------+");
+			System.out.println("| 1.  List all existing food trucks.         |");
+			System.out.println("| 2.  See the average rating of food trucks. |");
+			System.out.println("| 3.  Display the highest-rated food truck.  |");
+			System.out.println("| 4.  Quit the program.                      |");
+			System.out.println("+--------------------------------------------+");
+			System.out.print("+ Enter Selection -> ");
+			
+			int input = kb.nextInt();
+			
+			switch(input) {
+			case 1:
+				System.out.println();
+				System.out.println("+----------------------------------+");
+				System.out.println("| LIST OF ALL EXISTING FOOD TRUCKS |");
+				System.out.println("+----------------------------------+");
+				for(int i=0; i<FoodTruck.getNumTrucks(); i++) {
+					if(foodTruck[i].getName().length() > 0) {
+						System.out.println(foodTruck[i].toString());
+					}
+					else
+						continue;
+				}	
+				break;
+			case 2:
+				System.out.println();
+				System.out.println("+----------------------------------+");
+				System.out.println("|          AVERAGE RATING          |");
+				System.out.println("+----------------------------------+");
+				
+				double sum = 0;
+				
+				for (int i=0; i<FoodTruck.getNumTrucks(); i++) {
+					sum += foodTruck[i].getRating();
+				}
+				double average = (sum / FoodTruck.getNumTrucks());
+				System.out.printf("| -> The average rating of all food trucks is %.2f%n.", average);
+				break;
+			case 3:
+				System.out.println();
+				System.out.println("+----------------------------------+");
+				System.out.println("|     HIGHEST RATED FOOD TRUCK     |");
+				System.out.println("+----------------------------------+");
+				FoodTruck highestRated = foodTruck[0];
+				for(int i=0; i<FoodTruck.getNumTrucks(); i++) {
+					if(foodTruck[i].getRating() > highestRated.getRating()) {
+						highestRated = foodTruck[i];
+					}
+				}
+				System.out.println("| -> The highest rated food truck is: ");
+				System.out.println("+----------------------------------+");
+				System.out.println(highestRated.toString());
+				break;
+			case 4:
+				looping = false;
+				break;
+			}
+		}
+		
+		
+		System.out.println("Program ended.");
+		kb.close();
 		
 	}
 }
